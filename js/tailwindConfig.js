b0e3bfaf6e1e65b53476d4fb593e1f606c91eb89
Palash Halder <halder.palash08@gmail.com>
2022-10-09 22:32:39 +0600

    upto banner design with responsive

---
+++ js/tailwindConfig.js
@@ -0,0 +1,15 @@
+tailwind.config ={
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FAE3B6',
+                secondary: '#FDBB57',
+                tertiary: 'CFA485',
+                fourth: '#ef4444'
+            },
+            fontFamily: {
+                primary: ['Poppins', 'sans-serif']
+            }
+        }
+    }
+}
